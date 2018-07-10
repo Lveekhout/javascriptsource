b7fcd04c9512951a8756ec4d6199207cec855fde
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2018-07-10 11:33:01 +0200

    Jira app aanpassingen

--- Jira/js/index.js
+++ Jira/js/index.js
@@ -1,7 +1,12 @@
+function getChromeVersion() {     
+    var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+    return raw ? parseInt(raw[2], 10) : false;
+}
 var xhr = new XMLHttpRequest()
+var issues
 function getJiraSync(url) {
     xhr.open("OPTIONS", url, false)
-    xhr.setRequestHeader("Accept", "application/json")
     xhr.setRequestHeader("Authorization", "Basic cGFydHlzY2VuZTpwcw==")
     xhr.send()
     xhr.open("GET", url, false)
@@ -10,9 +15,9 @@ function getJiraSync(url) {
     xhr.send()
     return JSON.parse(xhr.responseText)
 }
-function selectChange() {
-    table.getElementsByTagName("tbody")[0].innerHTML = ""
-    let issues = getJiraSync("https://jira.intra.tkppensioen.nl/rest/agile/1.0/board/3373/sprint/" + select.value + "/issue").issues
+function sortKey() { issues.sort((a,b) => a.key.localeCompare(b.key)) }
+function sortSummary() { issues.sort((a,b) => a.fields.summary.localeCompare(b.fields.summary)) }
+function sortEpic() {
     issues.sort((a,b) => {
         if (a.fields.epic)
             if (b.fields.epic) return a.fields.epic.id-b.fields.epic.id
@@ -21,11 +26,22 @@ function selectChange() {
             if (b.fields.epic) return 1
             else return 0
     })
+}
+function sortReporter() { issues.sort((a,b) => a.fields.reporter.displayName.localeCompare(b.fields.reporter.displayName)) }
+function sortAssignee() { issues.sort((a,b) => a.fields.assignee.displayName.localeCompare(b.fields.assignee.displayName)) }
+function sortSubstatus() { issues.sort((a,b) => a.fields.status.statusCategory.name.localeCompare(b.fields.status.statusCategory.name)) }
+function toonIssues() {
+    table.getElementsByTagName("tbody")[0].innerHTML = ""
     issues.forEach(v => {
         let epic = ""
         if (v.fields.epic) epic = v.fields.epic.key
-        table.getElementsByTagName("tbody")[0].innerHTML += "<tr onclick=\"rowClick('" + v.key + "')\"><td>" + v.key + "</td><td>" + v.fields.summary + "</td><td>" + epic + "</td><td>" + v.fields.reporter.displayName + "</td><td>" + v.fields.assignee.displayName + "</td><td>" + v.fields.status.statusCategory.name + "</td></tr>"
+        table.getElementsByTagName("tbody")[0].innerHTML += "<tr onclick=\"rowClick('" + v.key + "')\"><td><a href='https://jira.intra.tkppensioen.nl/browse/" + v.key + "'>" + v.key + "</a></td><td>" + v.fields.summary + "</td><td>" + (v.fields.customfield_10302==null?"":v.fields.customfield_10302) + "</td><td>" + epic + "</td><td>" + v.fields.reporter.displayName + "</td><td>" + v.fields.assignee.displayName + "</td><td>" + v.fields.status.statusCategory.name + "</td></tr>"
     })
+}
+function selectChange() {
+    issues = getJiraSync("https://jira.intra.tkppensioen.nl/rest/agile/1.0/board/3373/sprint/" + select.value + "/issue").issues
+    sortEpic()
+    toonIssues()
 }
 function rowClick(key) {
     let issue = getJiraSync("https://jira.intra.tkppensioen.nl/rest/agile/latest/issue/" + key)
