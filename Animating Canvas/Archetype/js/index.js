c16b04071248615441f0db75a3412192bc90c8d1
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2018-07-23 14:35:38 +0200

    Is beter

--- Animating Canvas/Archetype/js/index.js
+++ Animating Canvas/Archetype/js/index.js
@@ -1,18 +1,22 @@
 let canvas
 let ctx
 function update() {
-    p001.innerHTML = window.requestAnimationFrame(draw)
+    //TODO: update situatie
 }
 
 function draw(millisec) {
+    update()
+
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     ctx.fillText(millisec, 5, 15)
-    setTimeout(update, 0)
+
+    p001.innerHTML = window.requestAnimationFrame(draw)
 }
 
 window.onload = () => {
     canvas = document.getElementById('canvas001')
     ctx = canvas.getContext('2d')
     ctx.font = "12pt Courrier new"
-    update()
+
+    draw()
 }
