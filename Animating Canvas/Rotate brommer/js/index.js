5ce2c3df67c89c2050dff105fd855db6f2fb37c0
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2018-07-26 12:06:08 +0200

    Met background

--- Animating Canvas/Rotate brommer/js/index.js
+++ Animating Canvas/Rotate brommer/js/index.js
@@ -1,17 +1,27 @@
+//http://codetheory.in/canvas-rotating-and-scaling-images-around-a-particular-point/
 let canvas
 let ctx
-let brommer
+let brommer, background
+let lfo = 0
 function update() {
     //TODO: update situatie
+    lfo += 0.1
+    if (lfo>Math.PI*2) lfo -= Math.PI*2
 }
 
 function draw(millisec) {
     update()
 
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+//    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.drawImage(background, 0, 0)
     ctx.fillText(millisec, 5, 15)
-    ctx.rotate(0.03)
-    ctx.drawImage(brommer, 0, 0)
+
+    ctx.save()
+    ctx.translate(brommer.width/2, brommer.height/2)
+    ctx.rotate(lfo)
+    ctx.scale(0.7+Math.sin(lfo)/2, 0.7+Math.sin(lfo)/2)
+    ctx.drawImage(brommer, -brommer.width/2, -brommer.height/2)
+    ctx.restore()
 
     p001.innerHTML = window.requestAnimationFrame(draw)
 }
@@ -21,8 +31,11 @@ window.onload = () => {
     ctx = canvas.getContext('2d')
     ctx.font = "12pt Courrier new"
 
+    background = new Image()
+    background.src = "image/Bremen_background.jpg"
+
     brommer = new Image()
     brommer.src = "image/hmijxsr-85743.png"
-
-    draw()
+    
+    brommer.addEventListener("load", () => p001.innerHTML = window.requestAnimationFrame(draw))
 }
