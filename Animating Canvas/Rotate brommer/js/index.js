37efb4eb63912df57ab0562f099963ba3ae256e2
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2018-07-27 09:52:17 +0200

    Background, football en LFO

--- Animating Canvas/Rotate brommer/js/index.js
+++ Animating Canvas/Rotate brommer/js/index.js
@@ -1,38 +1,74 @@
 //http://codetheory.in/canvas-rotating-and-scaling-images-around-a-particular-point/
 let canvas
 let ctx
-let brommer, background
-let lfo = 0
+let brommer, buggy
+let football = {scale: 0.05, posx: 0, posy: 0, speedx: 1/0.05, speedy: 0, image:new Image()}
+let lfo2 = new LFO(0.01)
+let lfo = new LFO(lfo2.value/10)
+let stop = false
 function update() {
-    //TODO: update situatie
-    lfo += 0.1
-    if (lfo>Math.PI*2) lfo -= Math.PI*2
+    lfo2.update()
+    lfo.speed = lfo2.value/10
+    lfo.update()
+    
+    football.speedy += 10
+    football.posx += football.speedx
+    football.posy += football.speedy
+    if (football.posx > ((canvas.clientWidth/football.scale)-football.image.width)) {
+        football.posx = 2*((canvas.clientWidth/football.scale)-football.image.width) - football.posx
+        football.speedx = -football.speedx
+    }
+    if (football.posx < 0) {
+        football.posx = -football.posx
+        football.speedx = -football.speedx
+    }
+    if (football.posy > ((canvas.clientHeight/football.scale)-football.image.height)) {
+        football.posy = 2*((canvas.clientHeight/football.scale)-football.image.height) - football.posy
+        football.speedy = -football.speedy
+    }
 }
 
 function draw(millisec) {
     update()
 
-//    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    ctx.drawImage(background, 0, 0)
-    ctx.fillText(millisec, 5, 15)
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    ctx.save()
+    ctx.translate(buggy.width/2, buggy.height/2)
+    ctx.rotate(-lfo.angle)
+    ctx.scale(0.7-lfo.value/2, 0.7-lfo.value/2)
+    ctx.drawImage(buggy, -buggy.width/2, -buggy.height/2)
+    ctx.restore()
 
     ctx.save()
     ctx.translate(brommer.width/2, brommer.height/2)
-    ctx.rotate(lfo)
-    ctx.scale(0.7+Math.sin(lfo)/2, 0.7+Math.sin(lfo)/2)
+    ctx.rotate(lfo.angle)
+    ctx.scale(0.7+lfo.value/2, 0.7+lfo.value/2)
     ctx.drawImage(brommer, -brommer.width/2, -brommer.height/2)
     ctx.restore()
 
-    p001.innerHTML = window.requestAnimationFrame(draw)
+    ctx.save()
+    ctx.scale(football.scale, football.scale)
+    ctx.drawImage(football.image, football.posx, football.posy)
+    ctx.restore()
+
+    if (!stop) p001.innerHTML = window.requestAnimationFrame(draw)
 }
 
 window.onload = () => {
     canvas = document.getElementById('canvas001')
-    ctx = canvas.getContext('2d')
-    ctx.font = "12pt Courrier new"
+    canvas.addEventListener("click", () => {
+        stop = !stop
+        if (!stop) p001.innerHTML = window.requestAnimationFrame(draw)
+    })
 
-    background = new Image()
-    background.src = "image/Bremen_background.jpg"
+    ctx = canvas.getContext('2d')
+ 
+    football.url = ""
+    football.image.src = "image/football.png"
+
+    buggy = new Image()
+    buggy.src = "image/4uom1wh-3cc80.png"
 
     brommer = new Image()
     brommer.src = "image/hmijxsr-85743.png"
