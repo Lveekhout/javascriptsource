86e639df5a08fd2e9acc05b76617a4345e608964
Laurens van Eekhout <leekhout@hotmail.com>
2018-10-05 11:23:58 +0200

    Ook iets met y richting

--- Animating Canvas/Force/js/Bolletje.js
+++ Animating Canvas/Force/js/Bolletje.js
@@ -1,12 +1,15 @@
 function Bolletje() {
     this.mass = 10
     this.position = [1,0]
-    this.speed = [0,0]
+    this.speed = [0,0.04]
     let force = 0.001
 
     this.update = () => {
         if (this.position[0] > 0) this.speed[0] -= force
         else this.speed[0] += force
+
+        if (this.position[1] > 0) this.speed[1] -= force
+        else this.speed[1] += force
 
         this.position[0] += this.speed[0]
         this.position[1] += this.speed[1]
