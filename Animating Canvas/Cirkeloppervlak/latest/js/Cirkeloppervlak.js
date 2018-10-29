a7e46d965e5507842d0169f6b52d78921f800335
Laurens van Eekhout <leekhout@hotmail.com>
2018-10-29 16:09:46 +0100

    Nieuwe versie. Nu correct

---
+++ Animating Canvas/Cirkeloppervlak/latest/js/Cirkeloppervlak.js
@@ -0,0 +1,83 @@
+function Cirkeloppervlak(canvas) {
+    canvas.addEventListener("mousewheel", e => {
+        let _zoom = zoom - e.deltaY/10
+        if (_zoom<10) _zoom = 10
+        if (_zoom>500) _zoom = 500
+        this.setZoom(_zoom)
+    })
+
+    let drag = false
+    canvas.addEventListener("mousedown", e => drag=true)
+    canvas.addEventListener("mouseup", e => drag=false)
+    canvas.addEventListener("mouseleave", e => drag=false)
+    canvas.addEventListener("mousemove", e => {
+        if (e.altKey) this.setOrigin(e.offsetX, e.offsetY)
+        else if (drag) this.setOrigin(x0+e.movementX, y0+e.movementY)
+    })
+
+    let ctx = canvas.getContext('2d')
+
+    let x0 = canvas.clientWidth / 5
+    let y0 = canvas.clientHeight / 1.2
+    let animate = false
+    let zoom = 95
+    let progress = 0
+
+    let raster = () => {
+        ctx.save()
+        ctx.lineWidth = 1
+        ctx.beginPath()
+            for (let x=x0+zoom; x<canvas.clientWidth; x+=zoom) { ctx.moveTo(x, 0); ctx.lineTo(x, canvas.clientHeight) }
+            for (let y=y0+zoom; y<canvas.clientHeight; y+=zoom) { ctx.moveTo(0, y); ctx.lineTo(canvas.clientWidth, y) }
+            for (let x=x0-zoom; x>0; x-=zoom) { ctx.moveTo(x, 0); ctx.lineTo(x, canvas.clientHeight) }
+            for (let y=y0-zoom; y>0; y-=zoom) { ctx.moveTo(0, y); ctx.lineTo(canvas.clientWidth, y) }
+            ctx.strokeStyle = "#CCC"
+        ctx.stroke()
+        ctx.beginPath()
+            ctx.moveTo(x0, 0)
+            ctx.lineTo(x0, canvas.clientHeight)
+            ctx.moveTo(0, y0)
+            ctx.lineTo(canvas.clientWidth, y0)
+            ctx.strokeStyle = "#F88"
+        ctx.stroke()
+        ctx.restore()
+    }
+    let draw = m => {
+        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight) // clear canvas
+
+        raster()
+
+        let x = Math.PI*2*(progress/1000)
+        ctx.beginPath()
+        ctx.moveTo(x0, y0)
+        ctx.arc(x0+x*zoom, y0-zoom, zoom, Math.PI/2, Math.PI/2-Math.PI*2*((1000-progress)/1000), true)
+        ctx.lineTo(x0+x*zoom, y0-zoom)
+        ctx.closePath()
+        ctx.fill()
+
+        ctx.fillText(new Date(), 5, 15)
+
+        if (animate) window.requestAnimationFrame(draw)
+    }
+    let paint = () => {
+        if (!animate) window.requestAnimationFrame(draw)
+    }
+    this.setZoom = v => {
+        zoom = v
+        paint()
+    }
+    this.setOrigin = (vx,vy) => {
+        x0 = vx
+        y0 = vy
+        paint()
+    }
+    this.setAnimate = v => {
+        animate = v
+        if (animate) window.requestAnimationFrame(draw)
+    }
+    this.setProgress = v => {
+        progress = v
+        if (!animate) window.requestAnimationFrame(draw)
+    }
+    paint()
+}
