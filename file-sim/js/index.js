8fff3b415519a812a3714599e98a6614295146b6
unknown <leekhout@hotmail.com>
2019-10-29 01:42:14 +0100

    Netter met functions

--- file-sim/js/index.js
+++ file-sim/js/index.js
@@ -6,9 +6,13 @@ let marge = 0.2//1124901351309333
 let status = {animate:true,error:""}
 let animate = true
 
-function beslis(a0, a1, c) {
-        if (c+a1.position-a0.position>0.5) a0.speed = 0.011
-        else a0.speed = 0.009
+function botsing(a) {
+    if (a.position>a.next.cc+a.next.position-marge) status.error = "botsing"
+}
+
+function beslis(a) {
+    if (a.next.cc+a.next.position-a.position>0.5) a.speed = 0.011
+    else a.speed = 0.009
 }
 
 function draw(millisec) {
@@ -17,34 +21,29 @@ function draw(millisec) {
 
     ctx.save() // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save
     ctx.translate(origin[0], origin[1])
-    for (i=0;i<autoos.length;i++) {
+    autoos.forEach(v => {
+        v.position += v.speed
+        v.speed += v.force
         ctx.save()
-        ctx.rotate(-autoos[i].position)
-        ctx.fillStyle = autoos[i].color
+        ctx.rotate(-v.position)
+        ctx.fillStyle = v.color
         ctx.fillRect(100, -10, 10, 20)
         ctx.restore()
-        autoos[i].position += autoos[i].speed
-        autoos[i].speed += autoos[i].force
 
-        if (i<autoos.length-1) {
-            if (autoos[i].position>autoos[i+1].position+autoos[i+1].speed-marge) status.error = "botsing"
-        } else {
-            if (autoos[i].position>Math.PI*2+autoos[0].position+autoos[0].speed-marge) status.error = "botsing"
-        }
+        v.botsing(v)
         //if (v.position>Math.PI*2) v.position -= Math.PI*2
-    }
+    })
     ctx.restore()
 
     if (status.error) throw status.error
 
     let now = new Date().getTime()
-    for (i=1;i<autoos.length;i++) {
-        if (now-autoos[i].time>100) {
-            autoos[i].time = now
-            if (i<autoos.length-1) beslis(autoos[i], autoos[i+1], 0)
-            else beslis(autoos[i], autoos[0], Math.PI*2)
+    autoos.forEach(v => {
+        if (now-v.time>100) {
+            v.time = now
+            if (v.beslis) beslis(v)
         }
-    }
+    })
 
     if (status.animate) window.requestAnimationFrame(draw)
 }
@@ -55,8 +54,22 @@ window.onload = () => {
     ctx.font = "12pt Courrier new"
 
     origin = [canvas.clientWidth/2, canvas.clientHeight/2]
-    for (i=0;i<Math.PI*2;i+=0.5+Math.random()) autoos.push({color:0,position:i,speed:0.01,force:0,time:0})
+    for (i=0;i<Math.PI*2;i+=0.5+Math.random()) autoos.push(
+        {
+            color: 0,
+            position: i,
+            cc: 0,
+            speed: 0.01,
+            force: 0,
+            time: 0,
+            botsing: botsing,
+            beslis: beslis
+        })
+    for (i=0;i<autoos.length-1;i++) autoos[i].next = autoos[i+1]
     autoos[0].color = "red"
+    autoos[0].cc = Math.PI*2
+    autoos[0].beslis = undefined
+    autoos[autoos.length-1].next = autoos[0]
 
     window.requestAnimationFrame(draw)
 }
